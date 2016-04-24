f9b859b0eed1a6ed837169fa566fba15424d29b7
Rahul Katariya <rahulkatariya@me.com>
2016-04-24 12:04:55 +0530

    Initial Commit

---
+++ Restofire-Gloss/Restofire-Gloss.h
@@ -0,0 +1,19 @@
+//
+//  Restofire-Gloss.h
+//  Restofire-Gloss
+//
+//  Created by Rahul Katariya on 24/04/16.
+//  Copyright © 2016 Rahul Katariya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Restofire-Gloss.
+FOUNDATION_EXPORT double Restofire_GlossVersionNumber;
+
+//! Project version string for Restofire-Gloss.
+FOUNDATION_EXPORT const unsigned char Restofire_GlossVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Restofire_Gloss/PublicHeader.h>
+
+
