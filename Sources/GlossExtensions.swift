52abe2634abab6bf01e888e71b12208d09692dfa
Rahul Katariya <rahulkatariya@me.com>
2016-04-28 13:20:57 +0530

    Update GlossExtensions.swift

--- Sources/GlossExtensions.swift
+++ Sources/GlossExtensions.swift
@@ -8,6 +8,76 @@
 
 import Gloss
 
+/**
+ Convenience operator for decoding JSON to array of Decodable objects
+ */
+public func <~~ <T: CollectionType where T.Generator.Element: Decodable>(key: String, json: JSON) -> T? {
+    return Decoder.decodeDecodableCollection(key)(json)
+}
+
+/**
+ Convenience operator for encoding array of Encodable objects to JSON
+ */
+public func ~~> <T: CollectionType where T.Generator.Element: Encodable>(key: String, property: T?) -> JSON? {
+    return Encoder.encodeEncodableCollection(key)(property)
+}
+
+extension Decoder {
+    /**
+     Returns function to decode JSON to value type
+     for objects that conform to the Decodable protocol
+     
+     :parameter: key JSON key used to set value
+     :parameter: keyPathDelimiter Delimiter used for nested keypath keys
+     
+     :returns: Function decoding JSON to an optional value type
+     */
+    public static func decodeDecodableCollection<T: CollectionType where T.Generator.Element: Decodable>(key: String, keyPathDelimiter: String = GlossKeyPathDelimiter()) -> JSON -> T? {
+        return {
+            json in
+            
+            if let jsonArray = json.valueForKeyPath(key, withDelimiter: keyPathDelimiter) as? [JSON] {
+                return T.fromJSONArray(jsonArray) as? T
+            }
+            
+            return nil
+            
+        }
+    }
+}
+
+extension Encoder {
+    
+    /**
+     Returns function to encode array as JSON
+     for objects the conform to the Encodable protocol
+     
+     :parameter: key Key used to create JSON property
+     
+     :returns: Function encoding array to optional JSON
+     */
+    public static func encodeEncodableCollection<T: CollectionType where T.Generator.Element: Encodable>(key: String) -> T? -> JSON? {
+        return {
+            array in
+            
+            if let array = array {
+                var encodedArray: [JSON] = []
+                
+                for model in array {
+                    if let json = model.toJSON() {
+                        encodedArray.append(json)
+                    }
+                }
+                
+                return [key : encodedArray]
+            }
+            
+            return nil
+        }
+    }
+    
+}
+
 public extension CollectionType where Generator.Element: Decodable {
     
     /**
